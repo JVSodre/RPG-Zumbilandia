12f376ff8a5efc22916c5a848cb3d74356ac35b5
JOAO VICTOR OLIVEIRA SODRE <02704906041@senacrs.edu.br>
2026-09-10 19:22:24 -0300

    Aula 05

--- Main.ts
+++ Main.ts
@@ -5,52 +5,35 @@ const teclado = prompt(); // variavel que permite a entrada de dados do usuario
 
 let personagem: Personagem | null = null; // variável global para armazenar o personagem criado
 
-function criarPersonagem() { // função para criar um novo personagem
+console.clear(); // limpa a tela do console
+console.log("===== Criando um novo personagem ====="); // mensagem informando que o processo de criação do personagem começou
+console.log(); 
+let nome = teclado("Digite o nome do seu personagem: "); // solicita o nome do personagem ao usuário
+let classe = teclado("Qual a classe do seu personagem: "); // solicita a classe do personagem ao usuário
 
-    console.clear(); // limpa a tela do console
-    console.log("===== Criando um novo personagem ====="); // mensagem informando que o processo de criação do personagem começou
-    console.log(); 
-    let nome = teclado("Digite o nome do seu personagem: "); // solicita o nome do personagem ao usuário
-    let classe = teclado("Qual a classe do seu personagem: "); // solicita a classe do personagem ao usuário
-    let nivel = 1; // define o nível inicial do personagem
-    let vida = 10; // define a vida inicial do personagem
-    let arma = "Livro de lógica - Bem legal"; // define a arma inicial do personagem
+personagem = new Personagem( // cria um novo objeto Personagem com os valores fornecidos pelo usuário
+    nome,
+    classe,
+); // armazena o novo personagem na variável global
 
-    let novo_personagem = new Personagem( // cria um novo objeto Personagem com os valores fornecidos pelo usuário
-        nome,
-        classe,
-        nivel,
-        vida,
-        arma
-    );
-
-    personagem = novo_personagem; // armazena o novo personagem na variável global
-}
 
 let escolha = 0; // variável para armazenar a escolha do usuário no menu
 
 while (escolha !== 9) { // loop principal do programa, que continua até o usuário escolher sair
 
-    console.clear(); // limpa a tela do console
     console.log("===== Menu ====="); 
     console.log(); // da um espaço em branco no console
-    console.log("1. Criar Personagem");
-    console.log("2. Mostrar resumo do personagem");
+    console.log("1. Mostrar resumo do personagem");
+    console.log("2. Treinar Ataque");
+    console.log("3. Curar Personagem");
+    console.log("4. Upar Personagem");
     console.log("9. Sair");
     console.log();
     escolha = +teclado("Escolha: "); // solicita a escolha do usuário no menu
 
     switch (escolha) {
 
-        case 1: // chama a função para criar um novo personagem
-            criarPersonagem();
-            console.log();
-            console.log("Personagem criado com sucesso!");
-            console.log();
-            teclado("Pressione qualquer tecla para continuar...");
-            break;
-
-        case 2: // exibe o resumo do personagem criado
+        case 1: // exibe o resumo do personagem criado
             if (personagem !== null) { // verifica se um personagem foi criado, se foi mostra o personagem, se não, exibe uma mensagem informando que ainda não foi criado
                 console.clear();
                 console.log("===== Resumo do Personagem =====");
@@ -64,10 +47,33 @@ while (escolha !== 9) { // loop principal do programa, que continua até o usuá
                 console.log();
                 teclado("Pressione qualquer tecla para continuar...");
             }
-            break;
+        break;
+
+        case 2: // treina ataque
+            try{
+                personagem.treinarAtaque()
+            } catch (erro) {
+                console.log((erro as Error).message)
+                escolha = 9;
+            }
+        break
+
+        case 3: 
+            try{
+                personagem.curarPersonagem()
+            } catch (erro) {
+                console.log((erro as Error).message)
+            }
+        break
+
+        case 4:
+            personagem.uparPersonagem()
+            console.log(`Você está no nível ${personagem.nivel} com ${personagem.vida} de vida e ${personagem.ataque} de ataque`)
+        break;
+
         case 9: // sai do loop e encerra o programa
             console.clear();
             console.log("Saindo...");
-            break;
+        break;
     }
 }
--- Personagem.ts
+++ Personagem.ts
@@ -7,21 +7,45 @@ export class Personagem { // classe (nome de classe primeiro caractere maiusculo
     classe: string;
     nivel: number;
     vida: number;
+    ataque: number;
     arma: string;
-
 
     constructor( // construto, função que é executada quando a classe é instanciada
         nome: string, // parâmetros do construtor, valores que precisam ser passados quando a classe é instanciada
         classe: string,
-        nivel: number,
-        vida: number,
-        arma: string
+        //vida: number,
+        //ataque: number,
+      
     ) {
         this.nome = nome; // retorna o valor do parâmetro para o atributo da classe
         this.classe = classe;
-        this.nivel = nivel;
-        this.vida = vida;
-        this.arma = arma;
+        this.nivel = 1;
+        this.vida = 75;
+        this.ataque = 25;
+        this.arma = "Livro de Lógica";
+    }
+
+    //métodos
+
+    treinarAtaque(): void{
+        this.ataque += 5+Math.floor(Math.random()*10);
+        this.vida -= 5+Math.floor(Math.random()*10);
+        if(this.vida <= 0){
+            throw new Error("Personagem morreu!")
+        }
+    }
+
+    curarPersonagem(): void{
+        this.vida += 5+Math.floor(Math.random()*15)
+        if (this.vida >= this.vida) {
+            throw new Error("Vida máxima!")
+        }
+    }
+
+    uparPersonagem(): void{
+        this.nivel += 1
+        this.vida += 75
+        this.ataque += 25
     }
 }
 
